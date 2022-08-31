8b9d21f0859a08292d70e1652effc1adb04a71ce
minhdl <longminhdo2000@gmail.com>
2022-08-31 17:40:21 +0700

    deploy be

--- src/utils/axiosWallet.ts
+++ src/utils/axiosWallet.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosWallet = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || '34.143.186.43:4000/',
+  baseURL: process.env.REACT_APP_API_URL || 'http://34.143.186.43:4000/',
   headers: {
     // Overwrite Axios's automatically set Content-Type
     'Content-Type': 'application/json',
