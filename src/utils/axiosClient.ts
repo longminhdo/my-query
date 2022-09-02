c6480993980ecf239be04e72810a99db9ce70f4d
minhdl <longminhdo2000@gmail.com>
2022-09-02 11:00:51 +0700

    update axios

--- src/utils/axiosClient.ts
+++ src/utils/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://34.143.186.43:6009/',
+  baseURL: process.env.REACT_APP_API_URL || 'http://34.126.94.246:6009/',
   headers: {
     // Overwrite Axios's automatically set Content-Type
     'Content-Type': 'application/json',
